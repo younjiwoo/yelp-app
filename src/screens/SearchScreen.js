c2165f6e9301c1e74d7d2285395a4cc213e32ed2
Younji <younjiwoo@gmail.com>
2022-02-23 16:58:12 +0900

    search screen (or the landing screen) finished

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useBusinesses from '../hooks/useBusinesses';
 import ResultsList from '../components/ResultsList';
@@ -11,20 +11,44 @@ const SearchScreen = () => {
 
 	console.log(results);
 
+	const filterResultsByPrice = (price) => {
+		// price === '$' || '$$' || '$$$'
+		return results.filter((result) => {
+			return result.price === price;
+		});
+	};
+
 	return (
-		<View>
+		<>
 			<SearchBar
 				term={term}
 				onTermChange={setTerm}
 				onTermSubmit={() => searchApi(term)}
 			/>
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
-			<Text>We have found {results.length} results.</Text>
-
-			<ResultsList title="Cost Effective" />
-			<ResultsList title="Bit Pricier" />
-			<ResultsList title="Big Spender" />
-		</View>
+			<ScrollView>
+				<ResultsList
+					results={filterResultsByPrice('$')}
+					title="Cost Effective"
+				/>
+				<ResultsList
+					results={filterResultsByPrice('$$')}
+					title="Bit Pricier"
+				/>
+				<ResultsList
+					results={filterResultsByPrice('$$$')}
+					title="Big Spender"
+				/>
+				<ResultsList
+					results={filterResultsByPrice('$$$')}
+					title="Big Spender"
+				/>
+				<ResultsList
+					results={filterResultsByPrice('$$$')}
+					title="Big Spender"
+				/>
+			</ScrollView>
+		</>
 	);
 };
 
